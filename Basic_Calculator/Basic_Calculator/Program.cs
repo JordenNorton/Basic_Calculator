e6c2a9ff7c48c6ab855c7d8719e52f67e2c32ca9
Jorden Norton <jordennorton@hotmail.co.uk>
2016-06-13 00:00:48 +0100

    Created basic calculator (Check comments). Only calculates and displays the answer for whole numbers.

--- Basic_Calculator/Basic_Calculator/Program.cs
+++ Basic_Calculator/Basic_Calculator/Program.cs
@@ -10,7 +10,54 @@ namespace Basic_Calculator
     {
         static void Main(string[] args)
         {
+            //Start of program ---
+            Console.WriteLine("Basic Calculator V1.0");
+            Console.WriteLine();
 
+            //Variables ---
+            int firstNum;
+            int secondNum;
+            int chosenOperator;
+
+
+            //Ask for first value ---
+            Console.WriteLine("Please enter your first number");
+            firstNum = Convert.ToInt32(Console.ReadLine());
+
+            //Ask for second value ---
+            Console.WriteLine("Please enter your second number");
+            secondNum = Convert.ToInt32(Console.ReadLine());
+
+            //Ask for operator ---
+            Console.WriteLine("Press 1 for +");
+            Console.WriteLine("Press 2 for -");
+            Console.WriteLine("Press 3 for *");
+            Console.WriteLine("Press 4 for /");
+
+            chosenOperator = Convert.ToInt32(Console.ReadLine());
+
+            //Calculation ---
+            switch (chosenOperator)
+            {
+                case 1:
+                    Console.WriteLine("{0} + {1} = {2}",firstNum, secondNum, firstNum + secondNum);
+                    break;
+                case 2:
+                    Console.WriteLine("{0} - {1} = {2}", firstNum, secondNum, firstNum - secondNum);
+                    break;
+                case 3:
+                    Console.WriteLine("{0} * {1} = {2}", firstNum, secondNum, firstNum * secondNum);
+                    break;
+                case 4:
+                    Console.WriteLine("{0} / {1} = {2}", firstNum, secondNum, firstNum / secondNum);
+                    break;
+
+            }
+
+            //End of program ---
+            Console.WriteLine();
+            Console.WriteLine("Press any key to exit ...");
+            Console.ReadKey();  
         }
     }
 }
